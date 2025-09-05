a7771a54609071e4d921c11eb7f063f8091799f6
Michael Bestas <mkbestas@lineageos.org>
2025-09-05 11:55:25 +0300

    cebu: Enable LiveDisplay adaptive backlight
    Change-Id: I3892e178b87b569805dd5e929e009d465bd63461

--- device.mk
+++ device.mk
@@ -33,6 +33,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# LiveDisplay
+$(call soong_config_set,livedisplay_sysfs,enable_ab,true)
+
 # Moto Camera 4
 TARGET_MOTCAMERA3 := cebu
 TARGET_USES_MOTCAMERA3 := true
